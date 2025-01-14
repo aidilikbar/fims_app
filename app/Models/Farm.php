f1cc7e1d5c120a60af3f3ceb3750d26a319eb5ca
aidil <aidil.ikbar@kpk.go.id>
2025-01-14 22:49:23 +0100

    update models

--- app/Models/Farm.php
+++ app/Models/Farm.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Farm extends Model
 {
-    //
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function fimsCrops()
+    {
+        return $this->hasMany(FimsCrop::class);
+    }
+
+    public function pasFields()
+    {
+        return $this->hasMany(PasField::class);
+    }
 }
--- app/Models/FimsFinance.php
+++ app/Models/FimsFinance.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class FimsFinance extends Model
 {
-    //
+    use HasFactory;
+
+    public function farm()
+    {
+        return $this->belongsTo(Farm::class);
+    }
 }
